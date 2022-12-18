b9d31bbf233620de83831ccb13cde71b3c5fd99e
Matteo Spada <matteo.spada@ymail.com>
2022-12-18 10:16:25 +0100

    Initial Commit

---
+++ LemonLicenseValidator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LemonLicenseValidator
+//
+//  Created by Matteo Spada on 18/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LemonLicenseValidator/LemonLicenseValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonLicenseValidatorApp.swift
+//  LemonLicenseValidator
+//
+//  Created by Matteo Spada on 18/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LemonLicenseValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
